8d68d0e0c9a59af56cd00702424df07239235803
halfmexican <halfmexicanhalfamazing@gmail.com>
2024-10-26 16:00:04 -0500

    use index

--- js/main.js
+++ js/main.js
@@ -3,23 +3,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 
-// Your web app's Firebase configuration (ensure this matches your firebaseConfig in other files)
 const firebaseConfig = {
-
   apiKey: "AIzaSyCTSAROvZAhn4omwyaR4xaLuRV1HTjKNHQ",
-
   authDomain: "crochet-corner-302a9.firebaseapp.com",
-
   projectId: "crochet-corner-302a9",
-
   storageBucket: "crochet-corner-302a9.appspot.com",
-
   messagingSenderId: "49405618553",
-
   appId: "1:49405618553:web:47ac966757f98b28a4e7a8"
 
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -38,7 +30,6 @@ fetch('data/patterns.json')
   })
   .catch(error => {
     console.error('Failed to load patterns:', error);
-    // Optionally, display an error message to the user
     displayError('Failed to load patterns: ' + error.message);
   });
 
@@ -47,13 +38,7 @@ function renderPatternGallery(patterns) {
   const gallery = document.getElementById('pattern-gallery');
   gallery.innerHTML = ''; // Clear any existing content
 
-  // Shuffle the patterns array
-  const shuffledPatterns = patterns.sort(() => 0.5 - Math.random());
-
-  // Get the first 3 patterns from the shuffled array
-  const selectedPatterns = shuffledPatterns.slice(0, 3);
-
-  selectedPatterns.forEach(pattern => {
+  patterns.forEach((pattern, index) => {
     // Create the pattern article
     const article = document.createElement('article');
     article.classList.add('pattern');
@@ -76,11 +61,12 @@ function renderPatternGallery(patterns) {
 
     // View Pattern Button
     const link = document.createElement('a');
-    link.href = `pattern.html?name=${encodeURIComponent(pattern.name)}`;
+    link.href = `pattern.html?index=${index}`;
     link.classList.add('button');
     link.textContent = 'View Pattern';
     article.appendChild(link);
 
+    // Append the article to the gallery
     gallery.appendChild(article);
   });
 }
@@ -89,6 +75,4 @@ function displayError(message) {
   const gallery = document.getElementById('pattern-gallery');
   gallery.innerHTML = `<p>${message}</p>`;
 }
-
-// At the bottom of main.js, add:
 export { auth };
