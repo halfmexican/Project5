1800bd8d8c62b63c388ab5db7656f1287b9a95bc
halfmexican <halfmexicanhalfamazing@gmail.com>
2024-10-27 11:35:09 -0500

    add logout button

--- js/authState.js
+++ js/authState.js
@@ -1,12 +1,23 @@
 function updateAuthButton() {
     const authLink = document.getElementById('authLink');
+    const logoutLink = document.getElementById('logoutLink');
     const isLoggedIn = localStorage.getItem('isLoggedIn');
-    
+
     if (isLoggedIn === 'true') {
-        authLink.style.display = 'none';
+        authLink.classList.add('hidden');
+        logoutLink.classList.remove('hidden');
     } else {
-        authLink.style.display = 'block';
+        authLink.classList.remove('hidden');
+        logoutLink.classList.add('hidden');
     }
 }
 
 document.addEventListener('DOMContentLoaded', updateAuthButton);
+
+document.getElementById('logoutLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    localStorage.setItem('isLoggedIn', 'false'); // Update the login state
+    updateAuthButton(); // Update the navbar links
+    // Optionally, redirect the user to the home or login page
+    window.location.href = '/';
+});
